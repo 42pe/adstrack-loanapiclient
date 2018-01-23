29d7167235cf07522374f4352268269069662762
Diego Ferreyra <diego@404.pe>
2018-01-23 14:17:45 -0500

    Fixed the Error messages and the date errors.

--- public/js/loanForm.js
+++ public/js/loanForm.js
@@ -103,10 +103,104 @@ $(document).ready(function() {
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('[name="data[applicants][0][DateOfBirth]"]').datepicker({
-        endDate: "-18y -1d",
-        // startView: 2,
-        maxViewMode: 3
-    });
-    $('form').validate();
+    $.validator.addMethod("realDate", function(value, element) {
+
+        function leapYear(year) {
+          return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+
+        var d,dArray,mm,dd,yyyy;
+
+        // check if valid date
+        try {
+            d = Date.parse(value);
+            dArray = value.split('/');
+            mm = parseInt(dArray[0]);
+            dd = parseInt(dArray[1]);
+            yyyy = parseInt(dArray[2]);
+        } catch (e) {
+            console.error(e);
+            return false
+        }
+
+        // check if month is valid
+        if (mm < 1 || mm > 12) {
+            return false;
+        }
+
+        // get the highest day it could be for that month in that year
+        var highestDay = 0;
+        if (leapYear(yyyy) && (mm === 2)) {
+            highestDay = 29;
+        } else {
+            if (mm === 2) {
+                highestDay = 28;
+            } else {
+                highestDay = [1,3,5,7,8,10,12].includes(mm) ? 31 : 30
+            }
+        }
+        var res = dd > 0 && dd <= highestDay;
+
+        console.log(dd,mm,yyyy,leapYear(yyyy),highestDay,res);
+
+        return res;
+
+    }, "Please enter a valid date!");
+
+    $("form").validate({ errorPlacement: function(error, element) {
+        error.insertAfter(element.parent());
+      }, rules: { "data[applicants][0][DateOfBirth]": { realDate: true } } });
 });
+
+// Array.includes polyfill
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value: function(searchElement, fromIndex) {
+
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      // 1. Let O be ? ToObject(this value).
+      var o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      var len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      var n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        // c. Increase k by 1. 
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
